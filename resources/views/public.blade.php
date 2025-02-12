9d283066437217eb41e3c27b8f83e234e0746c05
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-02-12 12:40:31 -0500

    Agregando tracking con metricool

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -46,6 +46,8 @@
       }
     </style>
   @endif
+
+  <script>function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"3517b44141a34815f5c1ebb765608760"})});</script>
 </head>
 
 <body>
